06b6c9aa4c66d529de8bbc501da95ed25cb6130b
cian murray <g00358011@atu.ie>
2024-01-10 10:40:26 +0000

    openfeign added and tested

--- src/main/java/ie/atu/userms/OrderFeignClient.java
+++ src/main/java/ie/atu/userms/OrderFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "orderms")
+@FeignClient(name = "orderms",url = "http://localhost:8080")
 public interface OrderFeignClient {
 
     @GetMapping("/order/{orderId}")
